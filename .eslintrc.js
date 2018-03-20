a66597694c5c2400277252b1d67ddbdfbf1c50e4
Mike Bifulco <mbifulco@live.com>
2018-03-20 14:21:53 -0400

    update deps and config for jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ["airbnb", "prettier"],
-  plugins: ["prettier"],
+  plugins: ["prettier", "jest"],
   rules: {
     "prettier/prettier": "error",
     "no-console": [0],
