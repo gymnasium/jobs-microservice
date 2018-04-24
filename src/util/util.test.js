7b63930bee1393ba78865737012f3310252afe01
Mike Bifulco <mbifulco@live.com>
2018-04-24 14:30:31 -0400

    add back in a working test

--- src/util/util.test.js
+++ src/util/util.test.js
@@ -1,2 +1,14 @@
 const util = require("./util");
 const { MARKETS } = require("./constants");
+
+it('returns the correct cifty for a given Lat/Long', ()=> {
+  it('returns correctly for Charlotte', () => {
+    const market = util.getMarketFromLatLong({
+      latitude:'35.2248813',
+      longitude:'-80.8474005',
+    });
+
+    market.name.shouldBe('Charlotte');
+    market.id.shouldBe(60);
+  });
+});
